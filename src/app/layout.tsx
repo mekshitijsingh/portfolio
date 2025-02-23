884d3823410331ac8cbfceb9835858c79a6a96bc
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2025-02-23 10:23:07 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,33 +2,34 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+// Load Geist Sans font
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+// Load Geist Mono font
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
-export const metadata = {
+// Define Metadata for the page
+export const metadata: Metadata = {
   title: "Kshitij Singh",
   description: "Portfolio of Kshitij Singh",
 };
 
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
